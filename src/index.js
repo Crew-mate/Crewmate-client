27d07f39d953a6ffe1fb72a63b3840c4995b3f7f
fire-irror <firenoah.m@gmail.com>
2024-02-17 20:33:36 +0900

    [feat] 로그인 회원가입 연결

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import "./index.css"
+import "./Login/Login.css"
+import "./SignUp/SignUp.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
