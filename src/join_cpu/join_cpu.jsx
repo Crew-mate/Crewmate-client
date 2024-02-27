32cb4d5409d2693e2da58414ee555b91f6aca039
dinmoy <imhyojin614@gmail.com>
2024-02-28 00:07:01 +0900

    [feat] 지원폼 api 연동

--- src/join_cpu/join_cpu.jsx
+++ src/join_cpu/join_cpu.jsx
@@ -2,61 +2,52 @@ import React, { useState } from "react";
 import Layout from "../Layout";
 import axios from "axios"; // Axios 라이브러리 import
 import "../join_cpu/join_cpu.css";
+import { Navigate } from "react-router";
 
 export default function JoinCpu() {
   const [name, setName] = useState("");
-  const [studentNumber, setStudentNumber] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [motivation, setMotivation] = useState("");
-  const [futureActivity, setFutureActivity] = useState("");
+  const [student_num, setstudent_num] = useState("");
+  const [phone_num, setphone_num] = useState("");
+  const [answer1, setanswer1] = useState("");
+  const [answer2, setanswer2] = useState("");
+  const [answer3, setanswer3] = useState("");
   const [errors, setErrors] = useState({});
 
-  const handleSubmission = () => {
+  const handleSubmission = async() => {
     const errors = {};
     if (!name) {
       errors.name = "이름을 입력하세요";
     }
-    if (!studentNumber || !/^\d+$/.test(studentNumber)) {
-      errors.studentNumber = "학번을 숫자로 입력하세요";
+    if (!student_num || !/^\d+$/.test(student_num)) {
+      errors.student_num = "학번을 숫자로 입력하세요";
     }
-    if (!phoneNumber || !/^\d+$/.test(phoneNumber)) {
-      errors.phoneNumber = "전화번호를 숫자로 입력하세요";
+    if (!phone_num || !/^\d+$/.test(phone_num)) {
+      errors.phone_num = "전화번호를 숫자로 입력하세요";
     }
-    if (!motivation) {
-      errors.motivation = "CPU 지원동기를 입력하세요";
+    if (!answer1) {
+      errors.answer1 = "CPU 지원동기를 입력하세요";
     }
-    if (!futureActivity) {
-      errors.futureActivity = "추후 활동을 입력하세요";
+    if (!answer2) {
+      errors.answer2 = "추후 활동을 입력하세요";
+    }if (!answer3) {
+      errors.answer2 = "자신을 색깔로 표현한다면?";
     }
 
     if (Object.keys(errors).length > 0) {
       setErrors(errors);
       return;
     }
-
-    
-    axios.post("http://localhost:5000/users/club", {
-      name,
-      studentNumber,
-      phoneNumber,
-      motivation,
-      futureActivity
-    })
-    
-    .then(response => {
-      console.log("서버 응답:", response.data);
-      
-      setName("");
-      setStudentNumber("");
-      setPhoneNumber("");
-      setMotivation("");
-      setFutureActivity("");
-  
-      setErrors({});
-    })
-    .catch(error => {
-      console.error("서버 요청 오류:", error);
-    });
+    const newApply={name,student_num,phone_num,answer1,answer2,answer3};
+    try{
+      const response=await axios.post('http://localhost:5000/apply_form',newApply);
+      if(response.status===201){
+        alert('동아리 지원을 완료하였습니다');
+        Navigate('/')
+      }
+    }catch(error){
+      alert('동아리 지원에 실패했습니다');
+      console.log(error);
+    }
   };
 
 
@@ -89,49 +80,61 @@ export default function JoinCpu() {
             className="join_number_input"
             type="text"
             placeholder="ex) 2109"
-            value={studentNumber}
+            value={student_num}
             onChange={(e) => {
-              setStudentNumber(e.target.value);
-              clearError("studentNumber");
+              setstudent_num(e.target.value);
+              clearError("student_num");
             }}
           />
-          {errors.studentNumber && <div className="error-message">{errors.studentNumber}</div>}
+          {errors.student_num && <div className="error-message">{errors.student_num}</div>}
           <div className="join_call">전화번호</div>
           <input
             className="join_call_input"
             type="text"
             placeholder="숫자만 입력해 주세요"
-            value={phoneNumber}
+            value={phone_num}
             onChange={(e) => {
-              setPhoneNumber(e.target.value);
-              clearError("phoneNumber");
+              setphone_num(e.target.value);
+              clearError("phone_num");
             }}
           />
-          {errors.phoneNumber && <div className="error-message">{errors.phoneNumber}</div>}
+          {errors.phone_num && <div className="error-message">{errors.phone_num}</div>}
           <div className="why_join">CPU 지원동기</div>
           <textarea
             className="why_join_input"
             type="text"
             placeholder="지원동기"
-            value={motivation}
+            value={answer1}
             onChange={(e) => {
-              setMotivation(e.target.value);
-              clearError("motivation");
+              setanswer1(e.target.value);
+              clearError("answer1");
             }}
           />
-          {errors.motivation && <div className="error-message">{errors.motivation}</div>}
+          {errors.answer1 && <div className="error-message">{errors.answer1}</div>}
           <div className="another_content">추후에 하고싶은 활동</div>
           <textarea
             className="another_content_input"
             type="text"
             placeholder="내용"
-            value={futureActivity}
+            value={answer2}
             onChange={(e) => {
-              setFutureActivity(e.target.value);
-              clearError("futureActivity");
+              setanswer2(e.target.value);
+              clearError("answer2");
             }}
           />
-          {errors.futureActivity && <div className="error-message">{errors.futureActivity}</div>}
+          {errors.answer2 && <div className="error-message">{errors.answer2}</div>}
+          <div className="another_content">자신을 색깔로 표현한다면?</div>
+          <textarea
+            className="another_content_input"
+            type="text"
+            placeholder="내용"
+            value={answer3}
+            onChange={(e) => {
+              setanswer3(e.target.value);
+              clearError("answer3");
+            }}
+          />
+          {errors.answer3 && <div className="error-message">{errors.answer3}</div>}
           <div className="joining_btn">
             <button className="joining" onClick={handleSubmission}>신청하기</button>
           </div>
