633a330091aedf96154c835554cff87a809a7a96
fire-irror <firenoah.m@gmail.com>
2024-03-13 00:48:12 +0900

    fix : 동아리 화면 완료

--- src/club/club_history.jsx
+++ src/club/club_history.jsx
@@ -1,12 +1,11 @@
 import Slider from "react-slick";
 import { useState } from "react";
-import './club_member.css'
+import './club_history.css'
 
-export default function Club_member_slider() {
+export default function Club_his_slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const settings = {
-    className: "center",
     infinite: true,
     centerPadding: "60px",
     slidesToShow: 5,
@@ -21,35 +20,33 @@ export default function Club_member_slider() {
 
 
   return (
-      <div className="club-slider-container">
-        <span>HISTORY</span>
+      <div className="his-slider-container">
         <Slider {...settings}>
-          
-          <div className="slide1">
+          <div className="s1">
             <h3>1</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>2</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>3</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>4</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>5</h3>
           </div>
           <div className="slide1">
             <h3>6</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>7</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>8</h3>
           </div>
-          <div className="slide1">
+          <div className="s1">
             <h3>9</h3>
           </div>
         </Slider>
